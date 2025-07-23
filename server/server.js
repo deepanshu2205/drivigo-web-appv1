74e5b78bd0f3bfdaddb7878b4ed2c639f20ac059
Deepanshu <deepanshu.22052004@gmail.com>
2025-07-23 20:28:52 +0530

    CORS error fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,26 @@ const authMiddleware = require('./authMiddleware');
 
 const app = express();
 app.use(express.json()); 
-app.use(cors());
+
+const cors = require('cors');
+
+const allowedOrigins = [
+  'http://localhost:3000', // For your local development
+  'https://drivigo-web-appv1.vercel.app/' // <-- PASTE YOUR VERCEL URL HERE
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 const port = 5000;
 
 // --- RAZORPAY CONFIGURATION ---
