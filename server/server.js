c5c640d21ef3668817d2b9773121ceee169d0e43
Deepanshu <161423891+deepanshu2205@users.noreply.github.com>
2025-07-31 14:08:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -19,8 +19,7 @@ const io = socketIo(server, {
   cors: {
     origin: [
       'http://localhost:3000',
-      'https://drivigo-web-appv1.vercel.app',
-      'https://drivigo-web-appv1-git-fork-ak5113-a4491d-deepanshu-drivigo-team.vercel.app/'
+      'https://drivigo-web-appv1.vercel.app'
     ],
     methods: ["GET", "POST"]
   }
